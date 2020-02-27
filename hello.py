a17fb62a00bb75c66630d8787ffe4276b2435334
claudeuwimana <cassa.claude.com>
2020-02-27 22:56:37 +0800

    updated project

--- hello.py
+++ hello.py
@@ -1,6 +1,8 @@
 print("Hello Cassa")
 name=input("what is your name")
 age=input("enter your age")
+ads=input("enter your address")
 print("your name is:",name)
 print("your age is:",age)
+print("Your address is",ads)
 
